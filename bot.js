54fe1c8e14363ea5bc4b0809910cf1c26e3e9e85
j3fr <44120413+j3fr@users.noreply.github.com>
2018-10-14 19:59:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -448,59 +448,6 @@ client.on('message', message => {
     .setThumbnail(heg.avatarURL);
     message.channel.send(id)
 }       });
-
-client.on('messageDelete', msg => {
-    if (msg.channel.type !== "text") return
-    if (msg.channel.topic && msg.channel.topic.includes("hano-modlog")) return;
-    exports.fire(`**#${msg.channel.name} | ${msg.author.tag}'s message was deleted:** \`${msg.content}\``, msg.guild)
-})
-
-client.on('messageUpdate', (msg, newMsg) => {
-    if (msg.content === newMsg.content) return
-    exports.fire(`**#${msg.channel.name} | ${msg.author.tag} edited their message:**\n**before:** \`${msg.content}\`\n**+after:** \`${newMsg.content}\``, msg.guild)
-})
-
-client.on('guildMemberUpdate', (old, nw) => {
-    let txt
-    if (old.roles.size !== nw.roles.size) {
-        if (old.roles.size > nw.roles.size) {
-            //Taken
-            let dif = old.roles.filter(r => !nw.roles.has(r.id)).first()
-            txt = `**${nw.user.tag} | Role taken -> \`${dif.name}\`**`
-        } else if (old.roles.size < nw.roles.size) {
-            //Given
-            let dif = nw.roles.filter(r => !old.roles.has(r.id)).first()
-            txt = `**${nw.user.tag} | Role given -> \`${dif.name}\`**`
-        }
-    } else if (old.nickname !== nw.nickname) {
-        txt = `**${nw.user.tag} | Changed their nickname to -> \`${nw.nickname}\`**`
-    } else return
-    exports.fire(txt, nw.guild)
-})
-
-client.on('roleCreate', (role) => {
-    exports.fire("**New role created**", role.guild)
-})
-
-client.on('roleDelete', (role) => {
-    exports.fire("**Role deleted -> `" + role.name + "`**", role.guild)
-})
-
-client.on('roleUpdate', (old, nw) => {
-    let txt
-    if (old.name !== nw.name) {
-        txt = `**${old.name} | Role name updated to -> \`${nw.name}\`**`
-    } else return
-    exports.fire(txt, nw.guild)
-})
-
-client.on('guildBanAdd', (guild, user) => {
-    exports.fire(`**User banned -> \`${user.tag}\`**`, guild)
-})
-
-client.on('guildBanRemove', (guild, user) => {
-    exports.fire(`**User unbanned -> \`${user.tag}\`**`, guild)
-})
 client.on('message', ( message ) => {
   if(message.author.bot) return;
 
