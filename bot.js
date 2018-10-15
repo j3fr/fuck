f06f6fa7470e2adcd47d60c7a0221e188b4b5275
j3fr <44120413+j3fr@users.noreply.github.com>
2018-10-15 14:32:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1004,9 +1004,7 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
 		logChannel.send(voiceLeave);
 	}
 });
-const Discord = require('discord.js');
-const fs = require('fs');
-const hero = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
+const client = new Discord.Client({disableEveryone: true, maxMessagesCache: 1});
 const config = { prefix: "#" };
 const tpoints = {};
 const vpoints = {};
