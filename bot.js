eec73e86f1e5a3ed89acf302a0de18f8f24d078f
j3fr <44120413+j3fr@users.noreply.github.com>
2018-10-16 21:12:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,100 @@ const moment = require('moment')
 const prefix = '#';
 
 
+	client.on('message', async msg => {
+	var prefix = "#";
+	var user = msg.author;
+			var a = msg.guild.roles.find("name", 'Agar');
+		if(!a){
+        a = await msg.guild.createRole({
+		  name: "Agar",
+          color: "#ffffff",
+          permissions:[]
+		})
+		
+        }
+	    var m = msg.guild.roles.find("name", 'Minecraft');
+	if(!m){
+        m =  await msg.guild.createRole({
+		  name: "Minecraft",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var f = msg.guild.roles.find("name", 'Fortnite');
+		if(!f){
+        f =  await msg.guild.createRole({
+		  name: "Fortnite",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var black = msg.guild.roles.find("name", 'Blacksquad');
+	if(!black){
+        black =  await msg.guild.createRole({
+		  name: "Blacksquad",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+
+		if (msg.content.startsWith(prefix +'addmerole')) {
+
+		if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+msg.channel.send(`يرحي اختيار رتبة اللعبة الذي تريدها \n1- لعبة اقاريو :agario: \n2- لعبة ماين كرافت :minecraft: \n3- لعبة فورت نايت :fortnite: \n4- لعبة بلاك سكواد :blacksquad\n5- الغاء ❌ \n6- **لديك60 ثانية للاختيار **\n<@${msg.author.id}>`).then(res => {     
+     res.react(':agario:').then(r=>{     
+     res.react(':minecraft:').then(r=>{
+     res.react(':fortnite:').then(r=>{
+     res.react(':blacksquad:').then(r=>{
+     res.react('❌').then(r=>{
+
+    let aaa = (reaction, user) => reaction.emoji.name === ':agario:' && user.id === msg.author.id;    
+    let mmm = (reaction, user) => reaction.emoji.name === ':minecraft:' && user.id === msg.author.id;
+    let fff = (reaction, user) => reaction.emoji.name === ':fortnite:' && user.id === msg.author.id;
+    let bbbb = (reaction, user) => reaction.emoji.name === ':blackquad:' && user.id === msg.author.id;
+    let ccc = (reaction, user) => reaction.emoji.name === '❌' && user.id === msg.author.id;
+
+    let aa = res.createReactionCollector(aaa, { maxMatches:1 , time: 20000 , });
+    let mm = res.createReactionCollector(mmm, { maxMatches:1 , time: 20000 , });
+    let ff = res.createReactionCollector(fff, { maxMatches:1 , time: 20000 , });
+    let bb = res.createReactionCollector(bbb, { maxMatches:1 , time: 20000 , });
+    let bl = res.createReactionCollector(bbbb,{ maxMatches:1 , time: 20000 , });
+    let cc = res.createReactionCollector(ccc, { maxMatches:1 , time: 20000 , });
+
+aa.on("collect", r => {
+    msg.guild.member(user.id).addRole(a);
+	msg.channel.send('`تم اعطائك رتبة للعبة Agar`');
+	msg.delete();
+	})
+mm.on("collect", r => {
+    msg.guild.member(user.id).addRole(m);
+	msg.channel.send('`تم اعطائك رتبة للعبة Mincraft `');
+	msg.delete();
+})
+ff.on("collect", r => {
+    msg.guild.member(user.id).addRole(f);
+	msg.channel.send('`تم اعطائك رتبة للعبة Fortnite `');
+	msg.delete();
+})
+bl.on("collect", r => {
+    msg.guild.member(user.id).addRole(black);
+	msg.channel.send('`تم اعطائك رتبة للعبة Blacksquad `');
+	msg.delete();
+})
+cc.on("collect", r => {
+	msg.delete();
+})
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 }
+	 });
+
+
 client.on('message', message => {
     let args = message.content.split(" ").slice(1);
 if (message.content.startsWith(prefix + 'clear')) {
