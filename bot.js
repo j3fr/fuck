12d1ae462520b4dd80ac4a36c9327113910427d2
j3fr <44120413+j3fr@users.noreply.github.com>
2018-10-20 11:09:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,7 +53,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+        message.channel.send(`#rep <@479469196380733470> ${x}`)
           .then(m => {
             count++;
           })
