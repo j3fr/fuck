1fdea948281dd47d322564d303d3110be793b327
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-03 17:36:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,76 +10,47 @@ client.on('ready', () => {
   console.log(`Users! [ " ${client.users.size} " ]`);
   console.log('=======================================')
 });
-
-
-
-
-
 client.on('message', message => {
     if(message.content === '2h4'){
         message.channel.send('#daily')
     }
 });
-
 client.on('message', message => {
     if(message.content === '2h4'){
         message.channel.send('#credits')
     }
 });
-
-
 client.on('message', message => {
     if(message.content === '2h4'){
         message.channel.send('#rep <@446020555808964619>')
     }
 });
-
-client.on('message', message => {
-    if(message.content === '2h4'){
-        message.channel.send('#rep <@446020555808964619>')
-    }
-});
-
-client.on('message', message => {
-    if(message.content === '2h4'){
-        message.channel.send('#rep <@446020555808964619>')
-    }
-});
-
-
-
-client.on('message', message => {
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`󠀀󠀀󠀀󠀀 ${x}`)
-          .then(m => {
-            count++;
-          })
-          
+    client.on('message', message => {
+        var prefix = "!";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
         }
-      }
-});
-
-const prefix = ['.']
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
+        
+       
+      });
+client.on('ready', async() => {
+var server = "500241063001194507"; // ايدي السررفر
+var channel = ['507560225772011521' , '503970977722531840' , '503970988136857610'];
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**󠀀󠀀󠀀󠀀  ,󠀀󠀀󠀀󠀀  ,󠀀󠀀󠀀󠀀  ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   ,   , **')
+    },305);
+})
 
 
-
-if (command == "say" ) {
-let rank = message.guild.member(message.author).roles.find('name', '.');
-if (!rank) return message.reply(' ')
-  message.channel.send(args.join("  "))
-    message.delete();
-  }
-});
 
 client.login(process.env.BOT_TOKEN); 
