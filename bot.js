5a4583d76ce006725ee5cec062a2e640292022a0
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-03 18:15:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,7 @@ client.on('message', message => {
        
       });
 client.on('message', message => {
-if (message.content === "!spam" + "<@" + `${client.user.id}` + ">") {
+if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
