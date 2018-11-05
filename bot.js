fc2508482396003a4127186f64855e327508a6de
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-05 14:36:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`. ${x}`)
+        message.channel.send(`.. ${x}`)
           .then(m => {
             count++;
           })
