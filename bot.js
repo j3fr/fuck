eb5163dda75da24ee4fa247623e2d07d64058f52
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-07 14:31:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ if (message.content === '!spam') {
 });
 client.on('ready', async() => {
 var server = "500241063001194507"; 
-var channel = "509204678207209472";//ايدي الروم
+var channel = "";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , **')
     },305);
