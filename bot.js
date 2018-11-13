9c3b10d793837b4d78ce0eccdb2e5156757e272a
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-13 18:41:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on('ready', async() => {
 var server = "500241063001194507"; 
 var channel = "509204678207209472";
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , **')
+    client.guilds.get(server).channels.get(channel).send('**Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , Stranger Spam , **')
     },305);
 })
 
