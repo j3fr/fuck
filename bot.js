0593cb2b9eb5254c25e967a728e8abf8480a27bb
j3fr <44120413+j3fr@users.noreply.github.com>
2018-11-14 18:12:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,5 +19,22 @@ var channel = "509204678207209472";
 })
 
 
-
+    client.on('message', message => {
+        var prefix = "!";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'say')) {
+            message.channel.send(''+x);
+                message.delete(999)
+        }
+        
+       
+      });
 client.login(process.env.BOT_TOKEN); 
